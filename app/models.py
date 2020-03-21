b938d99868ac3dac87727d7212b84d1ad74f2293
Mohammed Shoaib <shoaibbilal101@gmail.com>
2020-03-21 12:45:12 +0500

    1

---
+++ app/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+# Create your models here.
+class category(models.Model):
+    cid=models.AutoField(primary_key=True)
+    mobileappname=models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.mobileappname
+
+
+class product(models.Model):
+    pid= models.AutoField(primary_key=True)
+    appname= models.CharField(max_length=200)
+    category= models.ForeignKey(category, on_delete=models.CASCADE)
+    price= models.FloatField(max_length=1000,default=0.0)
+    
+
+    def __str__(self):
+        return self.appname
+
+class appinfo(models.Model):
+    sno = models.AutoField(primary_key=True)
+    img1name= models.CharField(max_length=255,default="")
+    img1=models.ImageField(upload_to='upload/',default="dummy.jpg")
+    img2name= models.CharField(max_length=255,default="")
+    img2=models.ImageField(upload_to='upload/',default="dummy.jpg")
+    img3name= models.CharField(max_length=255,default="")
+    img3=models.ImageField(upload_to='upload/',default="dummy.jpg")
+    img4name= models.CharField(max_length=255,default="")
+    img4=models.ImageField(upload_to='upload/',default="dummy.jpg")
+    img5name= models.CharField(max_length=255,default="")
+    img5=models.ImageField(upload_to='upload/',default="dummy.jpg")
+    
+    category= models.ForeignKey(category, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.category.mobileappname 
+
+class client(models.Model):
+    name=models.CharField(max_length=255,default="")
+    companyname=models.CharField(max_length=255,default="")
+    email=models.CharField(max_length=255,default="")
+    contact=models.IntegerField()
+    message=models.TextField()
+    appid=models.IntegerField(default=0)
+    appimg=models.ImageField(upload_to='upload/',default="")
+
+    def __str__(self):
+        return self.email
---
+++ app/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render,HttpResponse,redirect
+from .models import category,product,appinfo,client
+
+# Create your views here.
+def index(request):
+    if request.method=="POST":
+        appname=request.POST['appname']
+        cat=request.POST['cat']
+        
+        price=request.POST['price']
+        data=product(appname=appname,category=category.objects.get(mobileappname=cat),price=price)
+        data.save()
+        request.session['category'] = cat
+       
+   
+        f=appinfo.objects.filter(category__mobileappname=cat)
+       
+     
+        # return HttpResponse(f)
+        
+        return render(request,'index.html',{'data':f})
+      
+    data=category.objects.all()
+    return render(request,'form.html',{'data':data})
+    # return render(request,'form.html')
+
+def detail(request,id):
+    data=appinfo.objects.filter(sno=id)
+    return render(request,'contact.html',{'data':data})
+
+
+    
+    
+def mobile(request):
+    return render(request,'index.html')
+def contact(request):
+    
+    if request.method=="POST":
+        name=request.POST['name']
+        companyname=request.POST['companyname']
+        email=request.POST['email']
+        contact=request.POST['contact']
+        message=request.POST['message']
+        appid=request.POST['appid']
+        data=client(name=name,companyname=companyname,email=email,contact=contact,message=message,appid=appid)
+        data.save()
+        if data:
+            return redirect('https://logodesigning.org/')
+    return HttpResponse('404')
+    # return render(request,'contact.html')
